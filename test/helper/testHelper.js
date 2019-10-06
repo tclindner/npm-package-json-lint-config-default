2437e908c4f6fa8bc1ece02b966fc9eccc45fd2a
tclindner <tclindner@users.noreply.github.com>
2019-10-06 15:20:09 -0500

    Bump deps

--- test/helper/testHelper.js
+++
@@ -1,7 +0,0 @@
-const {NpmPackageJsonLint} = require('npm-package-json-lint');
-
-module.exports = function lint(packageJsonData, config) {
-  const npmPackageJsonLint = new NpmPackageJsonLint();
-
-  return npmPackageJsonLint.lint(packageJsonData, config);
-};
